ee8286b58af66bfbd1618d9001be626651232034
Ayushi Sharma <sharmaayushi62998@gmail.com>
2022-06-12 09:05:05 +0530

    modularity

--- Node/1_cp.js
+++ Node/1_cp.js
@@ -2,7 +2,13 @@
 /*child process  is a node module used to create subprocess within a script//
 //you can perform different task with your script*/
 
-const cp= require('child_process')/*pura child process cp mei aa chula hai*/
-console.log('Trying to open calculator')
-/*ab cp ko sare child process ka exess mil jayega  ab hum likhenge   cp.*/
-cp.execSync
+const cp= require('child_process')/*pura child process cp mei aa chula hai
+console.log('Trying to open calculator')*/
+/*ab cp ko sare child process ka exess mil jayega  ab hum likhenge   cp.
+cp.execSync('calc')
+console.log("Trying to open vscode")
+cp.execSync("code")
+cp.execSync('start chrome https://www.pepcoding.com/')*/
+
+let output=cp.execSync('node test.js')
+console.log('output-'+ output)
