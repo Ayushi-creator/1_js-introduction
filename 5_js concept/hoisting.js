ff071f17d1a465bdc9078a4cef82f1c53eb01e1a
Ayushi-creator <sharmaayushi62998@gmail.com>
2022-07-11 23:14:08 +0530

    concept

---
+++ 5_js concept/hoisting.js
@@ -0,0 +1,34 @@
+// var a=2
+// function greet(){
+//     console.log('hello')
+// }
+// console.log(a)
+// greet()
+// output=2
+// hello
+
+// console.log(a)
+
+// undefined with var 
+// initialization error with let and const
+// Temporal Dead Zone - it is just a area where if you try to access variables defined with 
+// let and const before their initialization you wont be able to do it.
+
+// greet()
+// var a=2
+// function greet(){
+//     console.log('hello')
+// }
+// output=undefined
+// hello
+
+
+
+// console.log(a)
+// greet()
+// var a=2
+// function greet(){
+//     console.log(a)
+// }
+// output=undefined
+// undefined
